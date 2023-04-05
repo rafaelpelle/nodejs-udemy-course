d68170997092491d8ef8a759d4cffa16e716641b
Rafael Pelle <rafapelle@gmail.com>
2023-04-05 15:48:54 -0300

    use 3rd party library

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,13 @@
 const fs = require('fs');
+const validator = require('validator');
 const messages = require('./messages');
 
 const fileName = 'file.txt';
 const {message1, message2} = messages;
-
 fs.writeFileSync(fileName, message1);
 fs.appendFileSync(fileName, message2);
+
+const email1 = 'rafapelle@gmail.com';
+const email2 = 'gmail.com';
+console.log(email1, validator.isEmail(email1));
+console.log(email2, validator.isEmail(email2));

445c64d094777f4f00ec08f6e0acf10de730b2b4
Rafael Pelle <rafapelle@gmail.com>
2023-05-03 18:08:34 -0300

    parameter validation

--- notes-app/app.js
+++ notes-app/app.js
@@ -14,10 +14,17 @@ const handleMapper = {
   remove: removeNote,
 };
 
-const {command, title, message} = yargs.argv;
+const { command, title, message } = yargs.argv;
+
+if (!title) {
+  console.log('parameter "title" is required.');
+} else if (!(command === "add" || command === "remove")) {
+  console.log(
+    'The "command" parameter is required and can be either "add" or "remove".'
+  );
+} else if (command === "add" && !message) {
+  console.log('parameter "message" is required to add a new note.');
+}
 
 const handler = handleMapper[command];
-
-if (handler) {
-  handler(title, message);
-}
+handler(title, message);
